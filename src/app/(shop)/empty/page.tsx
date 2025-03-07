232cc5ebc080c86eb18dd90978a26c14ce675f9d
Camilo Figueroa <cigotete@gmail.com>
2025-03-07 14:59:12 -0500

    Added EmptyCart UI scaffolding.

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,8 +1,24 @@
+import Link from 'next/link';
+import { IoCartOutline } from 'react-icons/io5';
 
-export default function Empty() {
+export default function EmptyCart() {
   return (
-    <div>
-      <h1>Empty Page</h1>
+    <div className="flex justify-center items-center h-[800px]">
+
+      <IoCartOutline size={ 80 } className="mx-5" />
+
+      <div className="flex flex-col items-center">
+        <h1 className="text-xl font-semibold">
+          Your cart is empty
+        </h1>
+
+        <Link 
+          href='/'
+          className="text-blue-500 mt-2 text-4xl"
+        >
+          Back to home page
+        </Link>
+      </div>
     </div>
   );
 }
