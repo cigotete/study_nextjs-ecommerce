0745dd58a5cec1c216f0d781bfd425dea97866b1
Camilo Figueroa <cigotete@gmail.com>
2025-03-17 09:46:03 -0500

    Added state to handle cart process (using Zustand).

---
+++ src/state/cart/cart-store.ts
@@ -0,0 +1,107 @@
+import type { CartProduct } from "@/interfaces";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface State {
+  cart: CartProduct[];
+
+  getTotalItems: () => number;
+  getSummaryInformation: () => {
+    subTotal: number;
+    tax: number;
+    total: number;
+    itemsInCart: number;
+  };
+
+  addProductTocart: (product: CartProduct) => void;
+  updateProductQuantity: (product: CartProduct, quantity: number) => void;
+  removeProduct: (product: CartProduct) => void;
+}
+
+export const useCartStore = create<State>()(
+  persist(
+    (set, get) => ({ //Set makes a broadcast to components consuming respective state parts.
+      cart: [],
+
+      // Methods
+      getTotalItems: () => {
+        const { cart } = get();
+        return cart.reduce((total, item) => total + item.quantity, 0);
+      },
+
+      getSummaryInformation: () => {
+        const { cart } = get();
+
+        const subTotal = cart.reduce(
+          (subTotal, product) => (product.quantity * product.price) + subTotal,
+          0
+        );
+        const tax = subTotal * 0.15;
+        const total = subTotal + tax;
+        const itemsInCart = cart.reduce(
+          (total, item) => total + item.quantity,
+          0
+        );
+
+        return {
+          subTotal,
+          tax,
+          total,
+          itemsInCart,
+        };
+      },
+
+      addProductTocart: (product: CartProduct) => {
+        const { cart } = get();
+        console.log(cart);
+
+        // 1. Check if the product exists in the cart with the selected size.
+        const productInCart = cart.some(
+          (item) => item.id === product.id && item.size === product.size
+        );
+
+        if (!productInCart) {
+          set({ cart: [...cart, product] });
+          return;
+        }
+
+        // 2. Product exists by size, then increase.
+        const updatedCartProducts = cart.map((item) => {
+          if (item.id === product.id && item.size === product.size) {
+            return { ...item, quantity: item.quantity + product.quantity };
+          }
+
+          return item;
+        });
+
+        set({ cart: updatedCartProducts });
+      },
+
+      updateProductQuantity: (product: CartProduct, quantity: number) => {
+        const { cart } = get();
+
+        const updatedCartProducts = cart.map((item) => {
+          if (item.id === product.id && item.size === product.size) {
+            return { ...item, quantity: quantity };
+          }
+          return item;
+        });
+
+        set({ cart: updatedCartProducts });
+      },
+
+      removeProduct: (product: CartProduct) => {
+        const { cart } = get();
+        const updatedCartProducts = cart.filter(
+          (item) => item.id !== product.id || item.size !== product.size
+        );
+
+        set({ cart: updatedCartProducts });
+      },
+    }),
+
+    {
+      name: "shopping-cart",
+    }
+  )
+);
