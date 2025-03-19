e1bbd6643ab3a8436253b5d55c3de5dcf8af688e
Camilo Figueroa <cigotete@gmail.com>
2025-03-19 12:13:22 -0500

    Added register page.

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,5 +1,5 @@
 import { titleFont } from '@/config/fonts';
-import Link from 'next/link';
+import { RegisterForm } from './ui/RegisterForm';
 
 export default function Register() {
   return (
@@ -7,46 +7,7 @@ export default function Register() {
 
       <h1 className={ `${ titleFont.className } text-4xl mb-5` }>New account</h1>
 
-      <div className="flex flex-col">
-
-        <label htmlFor="email">Full name</label>
-        <input
-          className="px-5 py-2 border bg-gray-200 rounded mb-5"
-          type="text" />
-
-
-        <label htmlFor="email">Email</label>
-        <input
-          className="px-5 py-2 border bg-gray-200 rounded mb-5"
-          type="email" />
-
-
-        <label htmlFor="email">Password</label>
-        <input
-          className="px-5 py-2 border bg-gray-200 rounded mb-5"
-          type="email" />
-
-        <button
-          
-          className="btn-primary">
-          Create account
-        </button>
-
-
-        {/* divisor l ine */ }
-        <div className="flex items-center my-5">
-          <div className="flex-1 border-t border-gray-500"></div>
-          <div className="px-2 text-gray-800">Or</div>
-          <div className="flex-1 border-t border-gray-500"></div>
-        </div>
-
-        <Link
-          href="/auth/login" 
-          className="btn-secondary text-center">
-          Login
-        </Link>
-
-      </div>
+      <RegisterForm />
     </div>
   );
 }
