a5f8571f43e3b1a315ed0cc21d2c172ca8c8f66c
Camilo Figueroa <cigotete@gmail.com>
2025-03-27 15:12:39 -0500

    Uptaded UI for displaying orders by user.

--- src/app/(shop)/orders/page.tsx
+++ src/app/(shop)/orders/page.tsx
@@ -1,74 +1,81 @@
+export const revalidate = 0;
 // https://tailwindcomponents.com/component/hoverable-table
-import { Title } from '@/components';
-
-import Link from 'next/link';
-import { IoCardOutline } from 'react-icons/io5';
-
-export default function Orders() {
+import { getOrdersByUser } from "@/actions";
+import { Title } from "@/components";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import { IoCardOutline } from "react-icons/io5";
+export default async function OrdersPage() {
+  const { ok, orders = [] } = await getOrdersByUser();
+  if (!ok) {
+    redirect("/auth/login");
+  }
   return (
     <>
       <Title title="Orders" />
-
       <div className="mb-10">
         <table className="min-w-full">
           <thead className="bg-gray-200 border-b">
             <tr>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+              >
                 #ID
               </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                Full name
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+              >
+                Full Name
               </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+              >
                 Status
               </th>
-              <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+              <th
+                scope="col"
+                className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+              >
                 Options
               </th>
             </tr>
           </thead>
           <tbody>
-
-            <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-
-              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
-              <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                Mark
-              </td>
-              <td className="flex items-center text-sm  text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-
-                <IoCardOutline className="text-green-800" />
-                <span className='mx-2 text-green-800'>Paid</span>
-
-              </td>
-              <td className="text-sm text-gray-900 font-light px-6 ">
-                <Link href="/orders/123" className="hover:underline">
-                  Order details
-                </Link>
-              </td>
-
-            </tr>
-
-            <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-
-              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
-              <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                Mark
-              </td>
-              <td className="flex items-center text-sm  text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-
-                <IoCardOutline className="text-red-800" />
-                <span className='mx-2 text-red-800'>Unpaid</span>
-
-              </td>
-              <td className="text-sm text-gray-900 font-light px-6 ">
-                <Link href="/orders/123" className="hover:underline">
-                  Order details
-                </Link>
-              </td>
-
-            </tr>
-
+            {orders.map((order) => (
+              <tr
+                key={order.id}
+                className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
+              >
+                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                  {order.id.split("-").at(-1)}
+                </td>
+                <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  {order.OrderAddress?.firstName} {order.OrderAddress?.lastName}
+                </td>
+                <td className="flex items-center text-sm  text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                  {order.isPaid ? (
+                    <>
+                      <IoCardOutline className="text-green-800" />
+                      <span className="mx-2 text-green-800">Paid</span>
+                    </>
+                  ) : (
+                    <>
+                      <IoCardOutline className="text-red-800" />
+                      <span className="mx-2 text-red-800">Unpaid</span>
+                    </>
+                  )}
+                </td>
+                <td className="text-sm text-gray-900 font-light px-6 ">
+                  <Link href={`/orders/${ order.id }`} className="hover:underline">
+                    View order
+                  </Link>
+                </td>
+              </tr>
+            ))}
+            
           </tbody>
         </table>
       </div>
