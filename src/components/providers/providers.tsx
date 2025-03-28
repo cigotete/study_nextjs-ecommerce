abb5d94fa07494e2de603ef4d6e4cf8759584b78
Camilo Figueroa <cigotete@gmail.com>
2025-03-27 20:42:17 -0500

    Updated providers to include Paypal provider of app bootstrap.

--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -1,16 +1,22 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export const Providers = ({ children }: Props) => {
-
   return (
-    <SessionProvider>
-      { children }
-    </SessionProvider>
-  )
+    <PayPalScriptProvider options={{
+      clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID ?? '',
+      intent: 'capture',
+      currency: 'COP',
+    }}>
+      <SessionProvider>
+        { children }
+      </SessionProvider>
+    </PayPalScriptProvider>
+  );
 }
