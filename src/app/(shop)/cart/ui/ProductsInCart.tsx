02ead1c3149a72ae802dae8af9304a7f044c3e4e
Camilo Figueroa <cigotete@gmail.com>
2025-03-31 23:45:13 -0500

    Replacing images of products with a component related.

--- src/app/(shop)/cart/ui/ProductsInCart.tsx
+++ src/app/(shop)/cart/ui/ProductsInCart.tsx
@@ -1,9 +1,8 @@
 'use client';
 import { useEffect, useState } from 'react';
 
-import Image from 'next/image';
 import { useCartStore } from '@/state';
-import { QuantitySelector } from '@/components';
+import { ProductImage, QuantitySelector } from '@/components';
 import Link from 'next/link';
 
 export const ProductsInCart = () => {
@@ -26,8 +25,8 @@ export const ProductsInCart = () => {
     <>
       {productsInCart.map((product) => (
         <div key={ `${ product.slug }-${ product.size }`  } className="flex mb-5">
-          <Image
-            src={`/products/${product.image }`}
+          <ProductImage
+            src={product.image}
             width={100}
             height={100}
             style={{
--- src/components/product/slideshow/ProductSlideshow.tsx
+++ src/components/product/slideshow/ProductSlideshow.tsx
@@ -12,7 +12,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/thumbs';
 
 import './slideshow.css';
-import Image from 'next/image';
+import { ProductImage } from '../product-image/ProductImage';
 
 interface Props {
   images: string[];
@@ -48,10 +48,10 @@ export const ProductSlideshow = ( { images, title, className }: Props ) => {
         {
           images.map( image => (
             <SwiperSlide key={ image }>
-              <Image
+              <ProductImage
                 width={ 1024 }
                 height={ 800 }
-                src={ `/products/${ image }` }
+                src={ image }
                 alt={ title }
                 className="rounded-lg object-fill"
               />
@@ -73,10 +73,10 @@ export const ProductSlideshow = ( { images, title, className }: Props ) => {
         {
           images.map( image => (
             <SwiperSlide key={ image }>
-              <Image
+              <ProductImage
                 width={ 300 }
                 height={ 300 }
-                src={ `/products/${ image }` }
+                src={ image }
                 alt={ title }
                 className="rounded-lg object-fill"
               />
