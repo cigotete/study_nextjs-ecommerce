b0d62076d6d68a84211d22d0af9dea85f46c7e05
Camilo Figueroa <cigotete@gmail.com>
2025-03-31 23:38:55 -0500

    Added interface contract to handle product images.

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -22,6 +22,12 @@ export interface CartProduct {
   image: string;
 }
 
+export interface ProductImage {
+  id: number;
+  url: string;
+  productId: string;
+}
+
 export type ValidCategory = 'men'|'women'|'kid'|'unisex';
 export type ValidSize = 'XS'|'S'|'M'|'L'|'XL'|'XXL'|'XXXL';
 export type ValidType = 'shirts'|'pants'|'hoodies'|'hats';
