03173942d98bb765b3cc6a400b78ec8dff37416d
Camilo Figueroa <cigotete@gmail.com>
2025-03-31 23:30:25 -0500

    Added actions to product management inside admin area.

---
+++ src/actions/product/delete-product-image.ts
@@ -0,0 +1,51 @@
+'use server';
+
+import prisma from '@/lib/prisma';
+import {v2 as cloudinary} from 'cloudinary';
+import { revalidatePath } from 'next/cache';
+cloudinary.config( process.env.CLOUDINARY_URL ?? '' );
+
+export const deleteProductImage = async( imageId: number, imageUrl: string ) => {
+
+  if ( !imageUrl.startsWith('http') ) {
+    return {
+      ok: false,
+      error: 'Can not be deleted images from filesystem'
+    }
+  }
+
+  const imageName = imageUrl
+    .split('/')
+    .pop()
+    ?.split('.')[0] ?? '';
+
+  try {
+
+    await cloudinary.uploader.destroy( imageName );
+    const deletedImage = await prisma.productImage.delete({
+      where: {
+        id: imageId
+      },
+      select: {
+        product: {
+          select: {
+            slug: true
+          }
+        }
+      }
+    })
+
+    // Revalidar los paths
+    revalidatePath(`/admin/products`)
+    revalidatePath(`/admin/product/${ deletedImage.product.slug }`);
+    revalidatePath(`/product/${ deletedImage.product.slug }`);
+
+  } catch (error) {
+    console.log(error);
+    return {
+      ok: false,
+      message: 'Can not be deleted image'
+    }
+  }
+
+}
