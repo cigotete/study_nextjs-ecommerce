7345df73652620c2d216f33a930bef525923f441
Camilo Figueroa <cigotete@gmail.com>
2025-03-31 23:49:22 -0500

    Updated sidebar with management pages inside admin area.

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -126,7 +126,7 @@ export const Sidebar = () => {
           <div className="w-full h-px bg-gray-200 my-10" />
 
           <Link
-            href="/"
+            href="/admin/products"
             className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
           >
             <IoShirtOutline size={ 30 } />
@@ -134,7 +134,7 @@ export const Sidebar = () => {
           </Link>
 
           <Link
-            href="/orders"
+            href="/admin/orders"
             onClick={() => closeMenu()}
             className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
           >
@@ -143,7 +143,7 @@ export const Sidebar = () => {
           </Link>
 
           <Link
-            href="/"
+            href="/admin/users"
             className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
           >
             <IoPeopleOutline size={ 30 } />
