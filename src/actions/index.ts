963ca4f97342059519c952a2bf4968ba3c132bf2
Camilo Figueroa <cigotete@gmail.com>
2025-03-31 23:34:08 -0500

    Added actions to the actions barrel file related to management for products, orders and users inside admin area.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -5,6 +5,10 @@ export * from './auth/register';
 export * from './product/product-pagination';
 export * from './product/get-product-by-slug';
 export * from './product/get-stock-by-slug';
+export * from './product/delete-product-image';
+export * from './product/create-update-product';
+
+export * from './category/get-categories';
 
 export * from './address/delete-user-address';
 export * from './address/get-user-address';
@@ -15,6 +19,10 @@ export * from './country/get-countries';
 export * from './order/place-order';
 export * from './order/get-order-by-id';
 export * from './order/get-orders-by-user';
+export * from './order/get-paginated-orders';
 
 export * from './payments/set-transaction-id';
 export * from './payments/paypal-check-payment';
+
+export * from './user/change-user-role';
+export * from './user/get-paginater-users';
